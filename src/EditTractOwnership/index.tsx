5485d994226db45be2bc14d30ad189f1b069c84a
César González <chesarglez429@gmail.com>
2021-01-14 02:24:56 -0600

    feat: Add table headers :sparkles:

--- src/EditTractOwnership/index.tsx
+++ src/EditTractOwnership/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 import { Tract } from '../Types';
+
+interface TableHeader {
+  text: string,
+  colSpan?: number,
+}
+
+const tableHeaders: TableHeader[] = [
+  {text: 'Owner'},
+  {text: 'Mineral Interest'},
+  {text: 'NPRI'},
+  {text: 'Lease', colSpan: 2},
+]
+
+const headersJSX = tableHeaders.map(({text, colSpan}) => (
+  <th key={text} colSpan={colSpan}> {text} </th>
+));
 
 const EditTractOwnership = ({ value = [], onChange = () => { } }: {value: Tract[], onChange?: Function}) => {
   return (
-    <h2>
-      TODO
-    </h2>
+    <Table>
+      <thead>
+        <tr>{headersJSX}</tr>
+      </thead>
+    </Table>
   );
 };
 
