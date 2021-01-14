22db3468ff86c9365b4ce5e40a1973dff0d14d4f
César González <chesarglez429@gmail.com>
2021-01-14 14:51:48 -0600

    style: format documents :gem:

--- src/EditTractOwnership/MineralInterestRow.tsx
+++ src/EditTractOwnership/MineralInterestRow.tsx
@@ -6,26 +6,24 @@ import { Form, Button } from 'react-bootstrap';
 import Icon from 'Icon';
 import InputWithAppendedText from 'atoms/InputWithAppendedText';
 
-const MineralInterestRow = ({value} : {value: MineralInterest}) => (
-    <tr>
-        <td>
-        <Form.Control type="text" value={value.owner} />
-        </td>
-        <td>
-        <InputWithAppendedText type="text" value={value.interest}>
-            %
-        </InputWithAppendedText>
-        </td>
-        <td>
-        {/* NPRI interest column */}
-        </td>
-        <td>
-        <Form.Control type="text" value={value.lease} />
-        </td>
-        <td>
-        <Button variant="light">{Icon({icon: 'remove'})}</Button>
-        </td>
-    </tr>
-)
+const MineralInterestRow = ({ value }: { value: MineralInterest }) => (
+  <tr>
+    <td>
+      <Form.Control type="text" value={value.owner} />
+    </td>
+    <td>
+      <InputWithAppendedText type="text" value={value.interest}>
+        %
+      </InputWithAppendedText>
+    </td>
+    <td>{/* NPRI interest column */}</td>
+    <td>
+      <Form.Control type="text" value={value.lease} />
+    </td>
+    <td>
+      <Button variant="light">{Icon({ icon: 'remove' })}</Button>
+    </td>
+  </tr>
+);
 
 export default MineralInterestRow;
--- src/EditTractOwnership/index.tsx
+++ src/EditTractOwnership/index.tsx
@@ -6,19 +6,21 @@ import { Table } from 'react-bootstrap';
 import MineralInterestRow from './MineralInterestRow';
 
 interface TableHeader {
-  text: string,
-  colSpan?: number,
+  text: string;
+  colSpan?: number;
 }
 
-const toThElement = ({text, colSpan}: TableHeader) => (
-  <th key={text} colSpan={colSpan}> {text} </th>
+const toThElement = ({ text, colSpan }: TableHeader) => (
+  <th key={text} colSpan={colSpan}>
+    {text}
+  </th>
 );
 
 const tableHeaders: TableHeader[] = [
-  {text: 'Owner'},
-  {text: 'Mineral Interest'},
-  {text: 'NPRI'},
-  {text: 'Lease', colSpan: 2},
+  { text: 'Owner' },
+  { text: 'Mineral Interest' },
+  { text: 'NPRI' },
+  { text: 'Lease', colSpan: 2 },
 ];
 
 const thElements = tableHeaders.map(toThElement);
@@ -27,15 +29,19 @@ const toMineralInterestRow = (mineralInterest: MineralInterest) => (
   <MineralInterestRow key={mineralInterest.id} value={mineralInterest} />
 );
 
-const EditTractOwnership = ({ value = [], onChange = () => { } }: {value: MineralInterest[], onChange?: Function}) => {
+const EditTractOwnership = ({
+  value = [],
+  onChange = () => {},
+}: {
+  value: MineralInterest[];
+  onChange?: Function;
+}) => {
   return (
     <Table>
       <thead>
         <tr>{thElements}</tr>
       </thead>
-      <tbody>
-        {value.map(toMineralInterestRow)}
-      </tbody>
+      <tbody>{value.map(toMineralInterestRow)}</tbody>
     </Table>
   );
 };
