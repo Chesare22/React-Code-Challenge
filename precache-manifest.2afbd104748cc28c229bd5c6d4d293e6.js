92ddf90acaa2f2ba573ec09054eb98d00cba2140
César González <chesarglez429@gmail.com>
2021-01-13 19:46:51 -0600

    Updates

---
+++ precache-manifest.2afbd104748cc28c229bd5c6d4d293e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae352ba75962208c7b5cd2eccef9f5e",
+    "url": "/React-Code-Challenge/index.html"
+  },
+  {
+    "revision": "5751c9802c33bff93c19",
+    "url": "/React-Code-Challenge/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "1234f2026ee67ea90ca3",
+    "url": "/React-Code-Challenge/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "5751c9802c33bff93c19",
+    "url": "/React-Code-Challenge/static/js/2.ef63f5f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Code-Challenge/static/js/2.ef63f5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1234f2026ee67ea90ca3",
+    "url": "/React-Code-Challenge/static/js/main.9673831a.chunk.js"
+  },
+  {
+    "revision": "483c40d811d5a6ba97bb",
+    "url": "/React-Code-Challenge/static/js/runtime-main.5e570a2d.js"
+  }
+]);
